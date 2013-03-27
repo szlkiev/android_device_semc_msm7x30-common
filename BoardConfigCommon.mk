3c2d407d3724dc7db4189396aa0f8e0e540bf043
Wechy77 <wechy77@gmail.com>
2013-03-27 21:09:36 +0100

    msm7x30-common: some cleanup * Removed useless & incorrect PRODUCT_MANUFACTURER definition * Added & fixed some comments to the source files * Deleted unused file recovery_ui.c * Clean up recovery_keys.c according to bootable/recovery/ * Move gsm feature xml to mogami-common & zeus * Remove custom RIL since it's merged in official cm10.1 * Revert init.sh modifications, no need for being different from official cm10 * Update releasetools
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I1e94132ca9491a34e8d5c817d639765401d60bd0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,92 +12,82 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This variable is set first, so it can be overridden
-# by BoardConfigVendor.mk
 USE_CAMERA_STUB := true
-BOARD_USES_GENERIC_AUDIO := false
-
 TARGET_SPECIFIC_HEADER_PATH := device/semc/msm7x30-common/include
 
-# Bootloader
 TARGET_NO_BOOTLOADER := true
 
 # Platform
 TARGET_BOARD_PLATFORM := msm7x30
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
-# Architecture
 TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
-
-# Scorpion optimizations
 TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
 
-# Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
-# Kernel information
-BOARD_KERNEL_CMDLINE := console=null
-BOARD_KERNEL_BASE    := 0x00200000
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_GPS := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+TARGET_QCOM_AUDIO_VARIANT := caf
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+TARGET_QCOM_DISPLAY_SYNC_FIX := true
+# BOARD_EGL_NEEDS_LEGACY_FB := true
+USE_OPENGL_RENDERER := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_NO_HW_VSYNC := true
+COMMON_GLOBAL_CFLAGS += -DSEMC_RGBA_8888_OFFSET
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_NO_SECURE_PLAYBACK
+BOARD_EGL_CFG := device/semc/msm7x30-common/prebuilt/egl.cfg
+BOARD_NEEDS_MEMORYHEAPPMEM := true
 
-# Custom boot
-TARGET_RECOVERY_PRE_COMMAND := "touch /cache/recovery/boot;sync;"
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/semc/msm7x30-common/releasetools/semc_ota_from_target_files
-BOARD_CUSTOM_BOOTIMG_MK := device/semc/msm7x30-common/custombootimg.mk
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/semc/msm7x30-common/recovery/recovery_keys.c
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_SMALL_RECOVERY := true
-BOARD_USES_RECOVERY_CHARGEMODE := false
-BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
-BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p1
-
-# Bluetooth
+BOARD_USES_GENERIC_AUDIO := false
 BOARD_HAVE_BLUETOOTH := true
+
+# Webkit
+TARGET_FORCE_CPU_UPLOAD := true
+ENABLE_WEBGL := true
+
+# Sensors
+SOMC_CFG_SENSORS := true
+
+# GPS
+BOARD_VENDOR_QCOM_AMSS_VERSION := 6225
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := semc
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
 # Camera
 #BOARD_USES_LEGACY_CAMERA := true
 BOARD_CPU_COLOR_CONVERT := true
 TARGET_DISABLE_ARM_PIE := true
 
-# GPS
-BOARD_USES_QCOM_GPS := true
-BOARD_USES_QCOM_LIBRPC := true
-BOARD_VENDOR_QCOM_AMSS_VERSION := 6225
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := semc
-BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+# Recovery
+BOARD_CUSTOM_BOOTIMG_MK := device/semc/msm7x30-common/custombootimg.mk
+TARGET_RECOVERY_PRE_COMMAND := "touch /cache/recovery/boot;sync;"
+BOARD_HAS_SMALL_RECOVERY := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_USES_RECOVERY_CHARGEMODE := false
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/semc/msm7x30-common/recovery/recovery_keys.c
+BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
+BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p1
 
-# Graphics
-BOARD_EGL_CFG := device/semc/msm7x30-common/prebuilt/egl.cfg
-TARGET_NO_HW_VSYNC := true
-TARGET_USES_C2D_COMPOSITION := true
-USE_OPENGL_RENDERER := true
-BOARD_EGL_NEEDS_LEGACY_FB := true
-BOARD_NEEDS_MEMORYHEAPPMEM := true
-#COMMON_GLOBAL_CFLAGS += -DSEMC_RGBA_8888_OFFSET -DANCIENT_GL
+# Kernel information
+BOARD_KERNEL_CMDLINE := console=null
+BOARD_KERNEL_BASE    := 0x00200000
 
-# QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_NO_SECURE_PLAYBACK
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_QCOM_DISPLAY_VARIANT := legacy
-
-# RIL
-BOARD_RIL_CLASS := ../../../device/semc/msm7x30-common/ril/
-
-# Sensor
-SOMC_CFG_DASH_INCLUDED := yes
-
-# Light Sensor
-BOARD_SYSFS_LIGHT_SENSOR := /sys/class/leds/lcd-backlight/als/enable
-
-# Webkit
-TARGET_FORCE_CPU_UPLOAD := true
-ENABLE_WEBGL := true
+# A custom ota package maker for a device without an exposed boot partition
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/semc/msm7x30-common/releasetools/semc_ota_from_target_files
 
 # Workaround to avoid issues with legacy liblights on QCOM platforms
 TARGET_PROVIDES_LIBLIGHT := true
+
+# Light Sensor
+BOARD_SYSFS_LIGHT_SENSOR := /sys/class/leds/lcd-backlight/als/enable
--- combinedroot/init.sh
+++ combinedroot/init.sh
@@ -29,38 +29,13 @@ busybox mount -t proc proc /proc
 busybox mount -t sysfs sysfs /sys
 busybox mount -t yaffs2 ${BOOTREC_CACHE} /cache
 
-# fixing CPU clocks to avoid issues in recovery
-busybox echo 1024000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-busybox echo 122000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-
-# trigger lime green LED & button-backlight
-busybox echo 25 > ${BOOTREC_LED_RED}
-busybox echo 255 > ${BOOTREC_LED_GREEN}
-busybox echo 0 > ${BOOTREC_LED_BLUE}
-busybox echo 255 > ${BOOTREC_LED_BUTTONS}
-busybox echo 50 > ${BOOTREC_VIBRATOR}
-busybox cat ${BOOTREC_EVENT} > /dev/keycheck&
-busybox sleep 1
-
-# trigger pink LED & button-backlight
-busybox echo 100 > ${BOOTREC_LED_RED}
-busybox echo 35 > ${BOOTREC_LED_GREEN}
-busybox echo 50 > ${BOOTREC_LED_BLUE}
-busybox echo 255 > ${BOOTREC_LED_BUTTONS}
-busybox echo 50 > ${BOOTREC_VIBRATOR}
-busybox cat ${BOOTREC_EVENT} > /dev/keycheck&
-busybox sleep 1
-
-# trigger aqua blue LED & button-backlight
-busybox echo 0 > ${BOOTREC_LED_RED}
-busybox echo 100 > ${BOOTREC_LED_GREEN}
+# trigger amber blue LED & button-backlight
+busybox echo 255 > ${BOOTREC_LED_RED}
+busybox echo 0 > ${BOOTREC_LED_GREEN}
 busybox echo 255 > ${BOOTREC_LED_BLUE}
 busybox echo 255 > ${BOOTREC_LED_BUTTONS}
 
-# trigger vibrator
-busybox echo 200 > ${BOOTREC_VIBRATOR}
-
-# keycheckblue
+# keycheck
 busybox cat ${BOOTREC_EVENT} > /dev/keycheck&
 busybox sleep 3
 
@@ -72,10 +47,10 @@ if [ -s /dev/keycheck -o -e /cache/recovery/boot ]
 then
 	busybox echo 'RECOVERY BOOT' >>boot.txt
 	busybox rm -fr /cache/recovery/boot
-	# trigger gold led
-	busybox echo 90 > ${BOOTREC_LED_RED}
-	busybox echo 255 > ${BOOTREC_LED_GREEN}
-	busybox echo 0 > ${BOOTREC_LED_BLUE}
+	# trigger blue led
+	busybox echo 0 > ${BOOTREC_LED_RED}
+	busybox echo 0 > ${BOOTREC_LED_GREEN}
+	busybox echo 255 > ${BOOTREC_LED_BLUE}
 	busybox echo 0 > ${BOOTREC_LED_BUTTONS}
 	# framebuffer fix
 	busybox echo 0 > /sys/module/msm_fb/parameters/align_buffer
